75de467382515894f6e3a9f6c6711f6b7313d059
DarkWolfZachary <95388123+DarkWolfZachary@users.noreply.github.com>
2024-02-26 17:16:29 -0800

    setup new ip adress and fixed online offline issues

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 function getOnlineStatus() {
   var request = new XMLHttpRequest();
 
-  request.open('GET', 'https://api.mcsrvstat.us/bedrock/3/51.81.164.5:27166', true);
+  request.open('GET', 'https://api.mcsrvstat.us/bedrock/3/51.81.150.31:19585', true);
   request.send(null);
-  request.onreadystatechange = function () {
+  request.onreadystatechange = function() {
     if (request.readyState === 4) {
 
       var type = request.getResponseHeader('Content-Type');
       if (type.indexOf("text") !== 1) {
         var output = JSON.parse(request.responseText);
-        var statusString;
-        var players = document.getElementsByClassName("players")[0];
-        players.textContent = (output.players.online)
-        var version = document.getElementsByClassName("version")[0];
-        version.textContent = (output.version)
-        var motd = document.getElementsByClassName("motd")[0];
-        motd.textContent = (output.map.clean.replace("§u", ''))
         var online = document.getElementsByClassName("online")[0];
+        var onlineOnly = document.getElementsByClassName("onlineOnly")[0];
         if (output.online) {
           online.textContent = 'Online';
+          var players = document.getElementsByClassName("players")[0];
+          players.textContent = (output.players.online)
+          var version = document.getElementsByClassName("version")[0];
+          version.textContent = (output.version)
+          var motd = document.getElementsByClassName("motd")[0];
+          motd.textContent = (output.map.clean.replace("§u", ''))
         } else {
           online.textContent = 'Offline';
+onlineOnly.classList.add("hidden");
         }
         setTimeout(() => {
           getOnlineStatus()
         }, 5000);
-
       }
     }
   }
 }
-
-addEventListener('load', (event) => {
-
-  getOnlineStatus();
+getOnlineStatus();
 const date1 = new Date();
 var MinFromMidnight = 7 * 60 - date1.getTimezoneOffset();
 var amOrpm;
@@ -46,12 +43,11 @@ if (MinFromMidnight > 0) {
 var time = ((7 * 60 - date1.getTimezoneOffset() + 12 * 60) / 60);
 
 var twelveHourTime
-if (time > 12 ){
-twelveHourTime = time - 12
-}else{
-twelveHourTime = time
+if (time > 12) {
+  twelveHourTime = time - 12
+} else {
+  twelveHourTime = time
 }
 
 var restartTime = document.getElementsByClassName("restartTime")[0];
 restartTime.textContent = '' + twelveHourTime + amOrpm + ' (local time).';
-})
