986d10bc13adca0b94c13a343002918a06f4f4cf
DarkWolfZachary <95388123+DarkWolfZachary@users.noreply.github.com>
2024-03-31 22:59:07 -0700

    dark mode for login screen

--- login/index.js
+++ login/index.js
@@ -6,3 +6,22 @@ function passwordVisible() {
       x.type = "password";
     }
   }
+
+  function setDarkMode() {
+    const body = document.querySelectorAll('body');
+    body[0].classList.add("dark");
+    body[0].classList.remove("light");
+  }
+
+  function setLightMode() {
+    const body = document.querySelectorAll('body');
+    body[0].classList.add("light");
+    body[0].classList.remove("dark");
+  }
+  
+  const isDarkMode = localStorage.getItem("theme") === "dark"
+  if (isDarkMode) {
+    setDarkMode()
+  }else{
+    setLightMode()
+  }
